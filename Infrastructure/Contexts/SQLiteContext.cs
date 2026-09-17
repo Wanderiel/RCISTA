4bfeaf7014b823b4d679e98183f660f77fd035a1
Pavel Dymov <dpavel82@yandex.ru>
2026-09-17 19:28:15 +0300

    Базовые слои

---
+++ Infrastructure/Contexts/SQLiteContext.cs
@@ -0,0 +1,12 @@
+﻿using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts;
+
+public class SQLiteContext : DbContext, IUnitOfWork
+{
+    public SQLiteContext(DbContextOptions<SQLiteContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
