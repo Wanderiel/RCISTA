97284cbfdc81d1039d34eb204dcc498f6892dc84
Pavel Dymov <dpavel82@yandex.ru>
2026-09-18 15:01:57 +0300

    подключаем DI

--- src/AppWPF/App.xaml.cs
+++ src/AppWPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
 
 namespace AppWPF;
 /// <summary>
@@ -6,5 +7,24 @@ namespace AppWPF;
 /// </summary>
 public partial class App : Application
 {
+    private IServiceProvider _serviceProvider;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        ServiceCollection services = new ServiceCollection();
+        ConfigureServices(services);
+        _serviceProvider = services.BuildServiceProvider();
+
+        MainWindow mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        // Регистрация сервисов
+        services.AddTransient<MainWindow>();
+    }
 }
 
